140a733438471e36ccec7fb9da9f07bffb24ac54
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2020-04-24 12:29:29 -0700

    Cleaning

--- FaceDetection/ViewController.swift
+++ FaceDetection/ViewController.swift
@@ -71,62 +71,54 @@ class ViewController: UIViewController {
    }
 
     let faceMask: UIButton = {
-            let button = UIButton()
-            button.translatesAutoresizingMaskIntoConstraints = false
-            button.addTarget(self, action: #selector(buttonToFaceMask(_:)), for: .touchUpInside)
-            button.setTitle("Face mask", for: .normal)
-    //        let icon = UIImage(systemName: "eye")?.resized(newSize: CGSize(width: 50, height: 30))
-    //        button.addRightImage(image: icon!, offset: 30)
-            button.backgroundColor = .systemGreen
-            button.layer.borderColor = UIColor.systemGreen.cgColor
-            button.layer.shadowOpacity = 0.3
-            button.layer.shadowColor = UIColor.systemGreen.cgColor
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(buttonToFaceMask(_:)), for: .touchUpInside)
+        button.setTitle("Face mask", for: .normal)
+        button.backgroundColor = .systemGreen
+        button.layer.borderColor = UIColor.systemGreen.cgColor
+        button.layer.shadowOpacity = 0.3
+        button.layer.shadowColor = UIColor.systemGreen.cgColor
 
-            return button
-        }()
+        return button
+    }()
 
-        let faceDetection: UIButton = {
-            let button = UIButton()
-            button.translatesAutoresizingMaskIntoConstraints = false
-            button.addTarget(self, action: #selector(buttonToFaceDetection(_:)), for: .touchUpInside)
-            button.setTitle("Face detection", for: .normal)
-    //        let icon = UIImage(systemName: "person.3.fill")?.resized(newSize: CGSize(width: 50, height: 25))
-    //        button.addRightImage(image: icon!, offset: 30)
-            button.backgroundColor = .systemOrange
-            button.layer.borderColor = UIColor.systemOrange.cgColor
-            button.layer.shadowOpacity = 0.3
-            button.layer.shadowColor = UIColor.systemOrange.cgColor
+    let faceDetection: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(buttonToFaceDetection(_:)), for: .touchUpInside)
+        button.setTitle("Face detection", for: .normal)
+        button.backgroundColor = .systemOrange
+        button.layer.borderColor = UIColor.systemOrange.cgColor
+        button.layer.shadowOpacity = 0.3
+        button.layer.shadowColor = UIColor.systemOrange.cgColor
 
-            return button
-        }()
+        return button
+    }()
 
-        let faceClassification: UIButton = {
-            let button = UIButton()
-            button.translatesAutoresizingMaskIntoConstraints = false
-            button.addTarget(self, action: #selector(buttonToFaceClassification(_:)), for: .touchUpInside)
-            button.setTitle("Face classification", for: .normal)
-    //        let icon = UIImage(systemName: "tray.fill")?.resized(newSize: CGSize(width: 50, height: 40))
-    //        button.addRightImage(image: icon!, offset: 30)
-            button.backgroundColor = .systemBlue
-            button.layer.borderColor = UIColor.systemBlue.cgColor
-            button.layer.shadowOpacity = 0.3
-            button.layer.shadowColor = UIColor.systemBlue.cgColor
+    let faceClassification: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(buttonToFaceClassification(_:)), for: .touchUpInside)
+        button.setTitle("Face classification", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.borderColor = UIColor.systemBlue.cgColor
+        button.layer.shadowOpacity = 0.3
+        button.layer.shadowColor = UIColor.systemBlue.cgColor
 
-            return button
-        }()
+        return button
+    }()
 
-        let objectDetection: UIButton = {
-            let button = UIButton()
-            button.translatesAutoresizingMaskIntoConstraints = false
-            button.addTarget(self, action: #selector(buttonToObjectDetection(_:)), for: .touchUpInside)
-            button.setTitle("Object detection", for: .normal)
-    //        let icon = UIImage(systemName: "crop")?.resized(newSize: CGSize(width: 50, height: 50))
-    //        button.addRightImage(image: icon!, offset: 30)
-            button.backgroundColor = .systemPurple
-            button.layer.borderColor = UIColor.systemPurple.cgColor
-            button.layer.shadowOpacity = 0.3
-            button.layer.shadowColor = UIColor.systemPurple.cgColor
+    let objectDetection: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(buttonToObjectDetection(_:)), for: .touchUpInside)
+        button.setTitle("Object detection", for: .normal)
+        button.backgroundColor = .systemPurple
+        button.layer.borderColor = UIColor.systemPurple.cgColor
+        button.layer.shadowOpacity = 0.3
+        button.layer.shadowColor = UIColor.systemPurple.cgColor
 
-            return button
-        }()
+        return button
+    }()
 }
